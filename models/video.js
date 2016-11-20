639763f65afc3f3b399c4fcf5c4cb79e7041757d
Marco <jmdc@my.codergv.com>
2016-11-20 01:32:41 -0600

    User and Video model setup

--- models/video.js
+++ models/video.js
@@ -2,7 +2,20 @@
 const mongoose = require('mongoose');
 
 let videoSchema = mongoose.Schema({
-  
+  url: {type: String, unique: true},
+  title: {type: String},
+  description: {type: String},
+  channel: {
+    title: {type: String},
+    url: {type: String}
+  },
+  published_at: {type: Date, default: Date.now},
+  tags: {type: Array},
+  comments: [{
+    username: {type: String},
+    comment_date: {type: Date, default: Date.now},
+    comment_body: {type: String}
+  }]
 });
 
 module.exports = mongoose.model('Video', videoSchema);
